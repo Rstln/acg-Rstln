9c8896d09dc10ed85721e40baa343e8d3efb366f
Rstln <50536545+Rstln@users.noreply.github.com>
2024-04-23 15:12:06 +0800

    task02 finished

--- task02/main.cpp
+++ task02/main.cpp
@@ -7,6 +7,10 @@
 //
 #include "parse_svg.h"
 
+#ifndef eps
+#define eps 1e-2
+#endif
+
 /***
  * signed area of a triangle connecting points (p0, p1, p2) in counter-clockwise order.
  * @param p0 1st point xy-coordinate
@@ -41,10 +45,83 @@ int number_of_intersection_ray_against_edge(
   auto a = area(org, org + dir, ps);
   auto b = area(org, pe, org + dir);
   auto c = area(org, ps, pe);
-  auto d = area(org + dir, ps, pe);
-  if (a * b > 0.f && d * c > 0.f && fabs(d) > fabs(c)) { return 1; }
+  auto d = area(dir + ps, ps, pe);
+  if (a * b > 0.f && d * c < 0.f) { return 1; }
   return 0;
 }
+
+
+float func(float c2, float c1, float c0, float t) {
+    return c2*t*t + c1*t + c0;
+}
+
+
+float dfunc(float c2, float c1, float t) {
+    return 2*c2*t + c1;
+}
+
+
+// Actually, roots formula for quadratic function is easier than those two methods :)
+float bisection(float c2, float c1, float c0, float left, float right) {
+    float l = 0, r = 1;
+    float mid;
+    while (abs(left-right) > eps) {
+        mid = (l + r) / 2;
+        float tmp = func(c2, c1, c0, mid);
+        if (tmp < eps) {
+            break;
+        }
+        else if (tmp * func(c2, c1, c0, r) > 0) {
+            r = mid;
+        }
+        else {
+            l = mid;
+        }
+    }
+    return mid;
+
+}
+
+
+float newton(float c2, float c1, float c0, float t0) {
+    float t1;
+    int itermax = 400, cnt = 0;
+    while (cnt < itermax) {
+        t1 = t0 - func(c2, c1, c0, t0) / dfunc(c2, c1, t0) / 2;
+        cnt++;
+        if (abs(t0-t1) < eps)
+            break;
+        else
+            t0 = t1;
+    }
+    return t1;
+}
+
+
+int check(float c2, float c1, float c0,
+          const Eigen::Vector2f &org,
+          const Eigen::Vector2f &dir,
+          const Eigen::Vector2f &ps,
+          const Eigen::Vector2f &pc,
+          const Eigen::Vector2f &pe) {
+    int cnt = 0;
+    float delta = sqrt(c1*c1-4*c2*c0);
+    float t1 = (-c1+delta)/2/c2;
+    if (t1 > 0 && t1 < 1) {
+        float s1 = (ps[0]*(1-t1)*(1-t1) + pc[0]*2*(1-t1)*t1 + pe[0]*t1*t1 - org[0]) * dir[0];
+        if (s1 > 0)
+            cnt++;
+    }
+    float t2 = (-c1-delta)/2/c2;
+    if (t2 > 0 && t2 < 1 && abs(t2-t1) > eps)  {
+        float s2 = (ps[0]*(1-t2)*(1-t2) + pc[0]*2*(1-t2)*t2 + pe[0]*t2*t2 - org[0]) * dir[0];
+        if (s2 > 0)
+            cnt++;
+    }
+    return cnt;
+
+}
+
 
 /***
  *
@@ -62,8 +139,24 @@ int number_of_intersection_ray_against_quadratic_bezier(
     const Eigen::Vector2f &pc,
     const Eigen::Vector2f &pe) {
   // comment out below to do the assignment
-  return number_of_intersection_ray_against_edge(org, dir, ps, pe);
+  // return number_of_intersection_ray_against_edge(org, dir, ps, pe);
   // write some code below to find the intersection between ray and the quadratic
+  float c2 = (ps[0] - 2*pc[0] + pe[0]) * (-dir[1]) + (ps[1] - 2*pc[1] + pe[1]) * dir[0];
+  float c1 = (-2*ps[0] + 2*pc[0]) * (-dir[1]) + (-2*ps[1] + 2*pc[1]) * dir[0];
+  float c0 = (ps[0] - org[0]) * (-dir[1]) + (ps[1] - org[1]) * dir[0];
+  if (c2 < 0) {
+      c2 = -c2;
+      c1 = -c1;
+      c0 = -c0;
+  }
+  float minv = c0 - c1*c1/4/c2;
+  if (minv > 0) {
+      return 0;
+  }
+  else {
+      return check(c2, c1, c0, org, dir, ps, pc, pe);
+  }
+
 }
 
 int main() {
